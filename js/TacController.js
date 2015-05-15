e0f5dc27ca1bbccb79a9f0513463fcd1e6c0b2ba
MerylHarden <merylharden@gmail.com>
2015-05-15 13:12:37 -0700

    added new objects with colors

--- js/TacController.js
+++ js/TacController.js
@@ -10,6 +10,7 @@ function TacController($firebaseObject){
     var self = this; 
     self.fbObject = getTest();
     self.clickMe = clickMe;
+    self.click2 = click2
 
     function getTest(){
         var ref = new Firebase("https://tictacapp.firebaseio.com/");
@@ -21,21 +22,39 @@ function TacController($firebaseObject){
     // self.fbObject.color = 'red';
         if(self.fbObject.color === 'red'){
             self.fbObject.color = 'green';
-            self.fbObject.blueColor = 'blue';
             self.fbObject.purpleColor = 'purple';
+            self.fbObject.orangeColor = 'orange';
+    
+
             
             // 7 - Whenever we change the object in any way, we MUST save it if we want it to change on firebase. 
-            self.fbObject.$save();
+            
         } else {
             self.fbObject.color = 'red';
-            self.fbObject.blueColor = 'yellow';
             self.fbObject.purpleColor = 'orange';
-            self.fbObject.
-            self.fbObject.$save();
+            self.fbObject.orangeColor = 'green';
+            
+            
 
         }
-
+        self.fbObject.$save();
 
     }
 
+
+    function click2(){
+        if(self.fbObject.blueColor ==='blue'){
+            self.fbObject.blueColor = 'yellow';
+        }else{
+            self.fbObject.blueColor ='blue';
+
+
+        }
+        self.fbObject.$save();
+    }
+
 }
+
+
+
+
